d50bd310fce271160014d44944e9d57a5fae06dc
DizzyDez <desc0137@algonquinlive.com>
2016-04-01 15:42:28 -0400

    Add Css, Js

--- js/main.js
+++ js/main.js
@@ -3,10 +3,38 @@ var $body = $('body');
 var $btnShowHide = $('.btn-show-hide');
 var $box = $('.box');
 
-var $btnMove = $('btn-move');
+var $btnMove = $('.btn-move');
 var $diamond = $('.diamond');
 
+var $btnCollapse = $('.btn-collapse-expand');
+var $panel = $('.panel');
+
+var $btnBounce = $('.btn-bounce');
+var $circle = $('.circle');
+
+var $btnAppend = $('.btn-append');
+var $list = $('.list');
 
 $btnShowHide.on('click', function () {
   $box.toggleClass('showbox');
+});
+
+$btnMove.on('click', function () {
+  $diamond.toggleClass('slide');
+});
+
+$btnCollapse.on('click', function () {
+  $panel.toggleClass('collapse');
+});
+
+$btnBounce.on('click', function () {
+  $circle.addClass('bounce');
+});
+
+$circle.on('animationend', function () {
+  $circle.removeClass('bounce');
+});
+
+$btnAppend.on('click', function () {
+  $list.prepend('<li>New List Item</li>');
 });
