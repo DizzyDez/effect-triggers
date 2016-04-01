e1d56ddab3b59546b4c5d237b6d91e53fe72cc3c
DizzyDez <desc0137@algonquinlive.com>
2016-04-01 12:34:30 -0400

    Yay finally got fade to work!

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var $body = $('body');
+
+var $btnShowHide = $('.btn-show-hide');
+var $box = $('.box');
+
+var $btnMove = $('btn-move');
+var $diamond = $('.diamond');
+
+
+$btnShowHide.on('click', function () {
+  $box.toggleClass('showbox');
+});
